789ec4932dec887e69625c7724f26ee9e9d9c407
Kachi Osuji <dikachiosuji9@gmail.com>
2024-08-12 18:47:21 +0100

    first commit

---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Creat = () => {
+  return (
+    <View>
+      <Text>Creat</Text>
+    </View>
+  )
+}
+
+export default Creat
